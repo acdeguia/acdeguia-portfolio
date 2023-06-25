a75453f79d68c16d3a723cbd031c4f5f459f4f37
Cate <anncatherinedeguia@gmail.com>
2023-06-25 13:46:21 +0800

    Add styles to projects section

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,21 +1,39 @@
-import { projects } from "../data/projects"
+import { projects } from "../data/projects";
 
 function Projects() {
-    return(
-        <section >
-            <h2>Deign and Coding Projects</h2>
-            <div>
-                {projects.map((project) => (
-                    <div>
-                        <img src={project.src} alt={project.name}/>
-                        <h3>{project.name}</h3>
-                        <p>{project.description}</p>
-                        <a href={project.link}>link</a>
-                    </div>
-                ))}
+  return (
+    <section className="container projects-section">
+      <h2 className="header_2">Design and Coding Projects</h2>
+      <div className="project-container">
+        {projects.map((project) => (
+          <div
+            className={`project ${project.id % 2 === 0 ? "even" : "odd"}`}
+            key={project.id}
+          >
+            <div className="image-container">
+              <img src={project.src} alt={project.name} />
+              <div
+                className={`proj-box-style${
+                  project.id % 2 === 0 ? "Even" : "Odd"
+                }`}
+              ></div>
             </div>
-        </section>
-    )
+            <div>
+              <h3>{project.name}</h3>
+              <p>{project.description}</p>
+              <a href={project.link}>
+                <p
+                  className={`link${project.id === "1" || project.id === '2' ? "Behance" : "Github"}`}
+                ></p>
+              </a>
+                
+              
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Projects
+export default Projects;
--- portfolio/src/data/projects.js
+++ portfolio/src/data/projects.js
@@ -6,8 +6,8 @@ import cvapp from '../assets/projects/cvapp.png'
 import memorycard from '../assets/projects/memorycardgame.png'
 
 export const projects = [
-    {id: '1', src:sagiptubig, link:'https://acdeguia.github.io/CV-app/', name:'SagipTubig', description:'A charity e-commerce website that aims to bring awareness to the world\'s comprehensive problem of trash floating on large bodies of water (beaches, rivers, and oceans).'},
-    {id: '2', src:ilungs, link:'https://acdeguia.github.io/CV-app/', name:'iLungs', description:'A web based machine learning- enabled lung health monitoring and referral system designed to connect patients with local health facilities.'},
+    {id: '1', src:sagiptubig, link:'https://www.behance.net/gallery/168306021/SagipTubig-E-commerce-website', name:'SagipTubig', description:'A charity e-commerce website that aims to bring awareness to the world\'s comprehensive problem of trash floating on large bodies of water (beaches, rivers, and oceans).'},
+    {id: '2', src:ilungs, link:'https://www.behance.net/gallery/168294129/IlUNGS-Symptom-Checker-Hospital-Finder', name:'iLungs', description:'A web based machine learning- enabled lung health monitoring and referral system designed to connect patients with local health facilities.'},
     {id: '3', src:greenoasis, link:'https://acdeguia.github.io/CV-app/', name:'Green Oasis', description:'I have created a landing page  using HTML, CSS, JavaScript, and webpack. It features a clean, user-friendly layout that captures the restaurant\'s atmosphere and menu.'},
     {id: '4', src:cvapp, link:'https://acdeguia.github.io/CV-app/', name:'CV Maker', description:'A CV app using React that enables users to easily create and download their CVs in PDF format. It has a user-friendly interface, allowing users to input their personal information, education, and work experience.'},
 ]
