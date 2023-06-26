851121fb4423cf68474caa5c16e3aacc09ef048c
Cate <anncatherinedeguia@gmail.com>
2023-06-26 12:11:09 +0800

    Fix typo

--- portfolio/src/components/MoreInfo.js
+++ portfolio/src/components/MoreInfo.js
@@ -8,7 +8,7 @@ function MoreInfo() {
       <h2 className="header_2">More Info & projects</h2>
       <p>
         
-      I started my journey as a web developer when I decided to study Computer Science in college. I've always been super interested in this stuff since I was a kid. I'm always curious and passionate about it, which makes me keep learning and getting better. I'm totally dedicated to growing as a person and in my career, always trying to find ways to learn more and improve my skills. My projects aren't flawless, but they are driven by my curiosity to learn new tools and tech. I've spent tons of hours designing, developing, and refining these projects, and they've really helped me improve my skills and knowledge.
+      I started my journey as a web developer when I decided to study Computer Science in college. I've always been interested in this stuff since I was a kid. I'm always curious and passionate about it, which makes me keep learning and getting better. I'm totally dedicated to growing as a person and in my career, always trying to find ways to learn more and improve my skills. My projects aren't flawless, but they are driven by my curiosity to learn new tools and tech. I've spent tons of hours designing, developing, and refining these projects, and they've really helped me improve my skills and knowledge.
       </p>
       <div className="moreProj-container">
         {moreProjects.map((project) => (
