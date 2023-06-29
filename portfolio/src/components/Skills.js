a61647d4c6ccfd52ed92a199f4b86f7e18978d7e
Cate <anncatherinedeguia@gmail.com>
2023-06-29 12:43:31 +0800

    Change skill section animation into carousel

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -1,24 +1,47 @@
-import skills from "../data/skills"
+import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import skills from '../data/skills';
+
+const carouselConfig = {
+    responsive: {
+      desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 4,
+      },
+      tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 5,
+      },
+      mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 4,
+      },
+    },
+    containerClass: 'carousel-container',
+    dotListClass: 'custom-dot-list-style',
+    itemClass: 'custom-skill-class',
+    keyBoardControl: true,
+    draggable: true,
+    swipeable: true,
+    infinite: true,
+    autoPlay: true,
+    autoPlaySpeed: 2000,
+  };
 
 function Skills() {
     return(
-        <section className="container skills-section">
-            <h2 className="header_2">Technical Skills</h2>
-            <div className="carousel-container">
-                <div className="skills-container">
-                    {skills.map((skill) =>(
-                        <div className="skill">
-                            <img src={skill.src} alt={skill.name}/>
-                            <p>{skill.name}</p>
-                        </div>
-                    ))}
-                </div>
-                <center>
-                    <br /><br /><br /><br />
-                    {/* <div>pagination conatiner</div> */}
-                </center>
+        <section className="skills-section">
+        <h2 className="header_2">Technical Skills</h2>
+        <Carousel {...carouselConfig}>
+          {skills.map((skill) => (
+            <div className="skill" key={skill.name}>
+              <img src={skill.src} alt={skill.name} />
+              <p>{skill.name}</p>
             </div>
-        </section>
+          ))}
+        </Carousel>
+      </section>
     )
 }
 
--- portfolio/src/data/skills.js
+++ portfolio/src/data/skills.js
@@ -3,6 +3,7 @@ import css from '../assets/skills/css.png'
 import js from '../assets/skills/js.png'
 import webpack from '../assets/skills/webpack.png'
 import react from '../assets/skills/react.png'
+import ts from '../assets/skills/typescript.png'
 import git from '../assets/skills/git.png'
 import firebase from '../assets/skills/firebase.png'
 import figma from '../assets/skills/figma.png'
@@ -15,11 +16,12 @@ const skills = [
     {id:'3', src: js, name:"JAVASCRIPT"},
     {id:'4', src: webpack, name:"WEBPACK"},
     {id:'5', src: react, name:"REACT"},
-    {id:'6', src: git, name:"GIT"},
-    {id:'7', src: firebase, name:"FIREBASE"},
-    {id:'8', src: figma, name:"FIGMA"},
-    {id:'9', src: adobeXd, name:"ADOBE XD"},
-    {id:'10', src: adobeIllustrator, name:"ILLUSTRATOR"}
+    {id:'6', src: ts, name:"TYPESCRIPT"}, 
+    {id:'7', src: git, name:"GIT"},
+    {id:'8', src: firebase, name:"FIREBASE"},
+    {id:'9', src: figma, name:"FIGMA"},
+    {id:'10', src: adobeXd, name:"ADOBE XD"},
+    {id:'11', src: adobeIllustrator, name:"ILLUSTRATOR"}
 ]
 
 export default skills
